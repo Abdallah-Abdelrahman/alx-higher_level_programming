8afdc82b38126e9bc253f67ddcb5f590f2dea1c3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-30 20:34:53 +0300

    Check cycle 010

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -17,24 +17,8 @@ int check_cycle(listint_t *list)
 	tortoise = list->next;
 	hare = tortoise->next;
 
-	for (; tortoise && hare; tortoise = tortoise->next, hare = hare->next->next)
+	for (;hare && hare->next; tortoise = tortoise->next, hare = hare->next->next)
 		if (hare == tortoise)
 			return (1);
 	return (0);
-}
-
-/**
- * _check_cycle - checks if a singly linked list has a cycle recursively
- * @h: pointer to head
- * @nnext: pointer to next next node
- *
- * Return: 0 if there is no cycle, 1 if there is a cycle
- */
-int _check_cycle(listint_t *h, listint_t *nnext)
-{
-	if (!h || !nnext)
-		return (0);
-	if (h == nnext)
-		return (1);
-	return (_check_cycle(h->next, nnext->next->next));
 }

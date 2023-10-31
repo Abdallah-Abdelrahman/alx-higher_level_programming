626fba9ffb3037ed7d33a3304fa2eaf805fb5593
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-10-31 23:04:24 +0300

    Update fizzbuzz 000

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * insert_node - write your short description
+ * Description: Long desc
+ *
+ * Return: 0 as exit status
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	return (0);
+}

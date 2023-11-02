9ae09c2ad09ffdd4c0fecfa123b2e5152ccc2dd5
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-02 21:30:19 +0300

    Print args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+length = len(argv)
+suffix = 'argument' if length > 1 else 'arguments'
+
+if (length - 1 == 0):
+    print("0 arguments.")
+else:
+    print('{:d} {:s}:'.format(length - 1, suffix))
+
+    for i in range(1, length):
+        print('{:d}: {:s}'.format(i, argv[i]))

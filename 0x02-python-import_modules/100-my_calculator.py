202c7ead1ab81f3d3f244d1e7ae66ffa9dd6068f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-03 00:41:31 +0300

    Handle operations 007

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,25 +3,25 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-if (__name__ == '__main__'):
+if __name__ == '__main__':
     length = len(argv)
 
-    if (length - 1 != 3):
+    if length - 1 != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    elif (argv[2] not in ['+', '-', '/', '*']):
+    if argv[2] not in ['+', '-', '/', '*']:
         print('Unknown operator. Available operators: +, -, * and /')
         exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
 
-        match argv[2]:
-            case '*':
-                print('{} * {} = {}'.format(a, b, mul(a, b)))
-            case '+':
-                print('{} + {} = {}'.format(a, b, add(a, b)))
-            case '/':
-                print('{} / {} = {}'.format(a, b, div(a, b)))
-            case '-':
-                print('{} - {} = {}'.format(a, b, sub(a, b)))
+    b = int(argv[3])
+    a = int(argv[1])
+
+    opt = argv[2]
+    dictionary = {
+       '*': lambda a, b: print('{} {} {} = {}'.format(a, opt, b, mul(a, b))),
+       '+': lambda a, b: print('{} {} {} = {}'.format(a, opt, b, add(a, b))),
+       '/': lambda a, b: print('{} {} {} = {}'.format(a, opt, b, div(a, b))),
+       '-': lambda a, b: print('{} {} {} = {}'.format(a, opt, b, sub(a, b))),
+    }
+
+    dictionary[argv[2]](a, b)
--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
+import os
 
+if __name__ == '__main__':
+    str ='#pythoniscool' 
+    os.write(1, "#pythoniscool")

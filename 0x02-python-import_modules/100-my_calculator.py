4770e100ffaece03c46168125f2cf08a3cdeae19
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-02 23:18:39 +0300

    Handle operations 003

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,18 +8,19 @@ if (__name__ == '__main__'):
 
     if (length - 1 != 3):
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
     elif (argv[2] not in ['+', '-', '/', '*']):
         print('Unknown operator. Available operators: +, -, * and /')
-
+        exit(1)
     else:
         a = argv[1]
         b = argv[3]
         match argv[2]:
             case '+':
-                print('{} + {} = {}'.format(a, b, add(a, b)))
+                print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
             case '/':
-                print('{} / {} = {}'.format(a, b, div(a, b)))
+                print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
             case '*':
-                print('{} * {} = {}'.format(a, b, mul(a, b)))
+                print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
             case '-':
-                print('{} - {} = {}'.format(a, b, sub(a, b)))
+                print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))

e3a3804e6c56c276642c540ae62c93df431af800
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-02 21:33:08 +0300

    Print argv 000

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,11 @@ from sys import argv
 length = len(argv)
 suffix = 'argument' if length > 1 else 'arguments'
 
-if (length - 1 == 0):
-    print("0 arguments.")
-else:
-    print('{:d} {:s}:'.format(length - 1, suffix))
+if __name__ == '__main__':
+    if (length - 1 == 0):
+        print("0 arguments.")
+    else:
+        print('{:d} {:s}:'.format(length - 1, suffix))
 
     for i in range(1, length):
         print('{:d}: {:s}'.format(i, argv[i]))

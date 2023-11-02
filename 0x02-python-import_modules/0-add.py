ef86a5abfc55f0c6ff12efacf727055a2f220f26
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-02 20:38:34 +0300

    add 007

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import add_0 as add
+
+from add_0 import add
+
 a = 1
 b = 2
-if __name__ != "0-add":
-    print("{:d} + {:d} = {:d}".format(a, b, add.add(a, b)))
+
+if (__name__ == "__main__"):
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

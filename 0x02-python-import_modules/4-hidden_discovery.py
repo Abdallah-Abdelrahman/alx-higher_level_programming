b9bb3667edf6809fa82ae6ab30cfa92a34f79ed9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-02 22:41:08 +0300

    Print names 000

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-from dis import dis
-with open('hidden_4.pyc', 'rb') as file:
-    bytecode = file.read()
 
-for str in dir(bytecode):
-    if (str.startswith('__')):
-        continue
-    print(str)
+from dis import dis
+
+if (__name__ == '__main__'):
+    with open('hidden_4.pyc', 'rb') as file:
+        bytecode = file.read()
+    for str in dir(bytecode):
+        if (str.startswith('__')):
+            continue
+        print(str)

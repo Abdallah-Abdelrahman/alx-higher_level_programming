4fe757b2c3c57f2ee88ba5d0f79cbe6bda49949e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-02 23:13:14 +0300

    Handle operations 000

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,17 +8,18 @@ if (__name__ == '__main__'):
 
     if (length - 1 != 3):
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    if (argv[2] not in ['+', '-', '/', '*']):
+    elif (argv[2] not in ['+', '-', '/', '*']):
         print('Unknown operator. Available operators: +, -, * and /')
 
-    a = argv[1]
-    b = argv[3]
-    match argv[2]:
-        case '+':
-            print('{} + {} = {}'.format(a, b, add(a, b)))
-        case '/':
-            print('{} / {} = {}'.format(a, b, div(a, b)))
-        case '*':
-            print('{} * {} = {}'.format(a, b, mul(a, b)))
-        case '-':
-            print('{} - {} = {}'.format(a, b, sub(a, b)))
+    else:
+        a = argv[1]
+        b = argv[3]
+        match argv[2]:
+            case '+':
+                print('{} + {} = {}'.format(a, b, add(a, b)))
+            case '/':
+                print('{} / {} = {}'.format(a, b, div(a, b)))
+            case '*':
+                print('{} * {} = {}'.format(a, b, mul(a, b)))
+            case '-':
+                print('{} - {} = {}'.format(a, b, sub(a, b)))

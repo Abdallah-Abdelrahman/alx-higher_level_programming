9a312db9916254a7b09db52b1218a97181a026b6
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-04 17:43:40 +0300

    Replace element in `c` fasion

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 
 def new_in_list(my_list, idx, element):
     new_list = my_list
+
     if (idx < 0 or len(my_list) <= idx):
         return (new_list)
     new_list[idx] = element
-    return (my_list)
+    return (new_list)

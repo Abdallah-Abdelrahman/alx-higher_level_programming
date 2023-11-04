457955249c1d7b75d428879ce121c30939b0c1ba
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-04 17:44:44 +0300

    Replace element w/out modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    new_list = my_list
+    new_list = my_list[:]
 
     if (idx < 0 or len(my_list) <= idx):
         return (new_list)

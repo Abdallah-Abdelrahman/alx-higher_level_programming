5ed6a03966908151cfbef20eaf7557ce85543c77
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-05 01:08:35 +0300

    Bool 003

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    if (not my_list):
-        return (None)
-    if (not len(my_list)):
-        return (my_list)
-    return [x % 2 == 0 for x in range(len(my_list))]
+    if (my_list != []):
+        return [x % 2 == 0 for x in range(len(my_list))]

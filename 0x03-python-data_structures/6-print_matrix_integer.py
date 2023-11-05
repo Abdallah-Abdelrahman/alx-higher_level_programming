0f2ca5c0a8d994b6d699b2103d4e2c0a71d432c1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-05 03:17:50 +0300

    Matrix 000

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if (not matrix or len(matrix) == 1):
+    if (not matrix or (len(matrix) == 1 and not matrix[0])):
         print()
         return
     for row in matrix:

04d8b71da865a2b67767479f099181a0ff699f8e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-08 15:35:28 +0300

    Use `set`

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,10 +4,4 @@ from functools import reduce
 
 
 def uniq_add(my_list=[]):
-    unique_list = []
-
-    for elem in my_list:
-        if elem not in unique_list:
-            unique_list.append(elem)
-
-    return (reduce(lambda acc, curr: acc + curr, unique_list))
+    return reduce(lambda acc, curr: acc + curr, set(my_list))

9095e5ad7157cfadc7ea268c98a8b60dd019e8cd
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-08 19:07:25 +0300

    Delete complex

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+    del_list = []
     if a_dictionary:
         for k, v in a_dictionary.items():
             if v == value:
-                del a_dictionary[k]
+                del_list.append(k)
+        for k in del_list:
+            del a_dictionary[k]
+    return a_dictionary

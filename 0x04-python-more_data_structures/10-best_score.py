934e3095a594cbde73b12143a749f88c48362515
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-08 17:29:29 +0300

    Fix dict iteration

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,11 @@
 def best_score(a_dictionary):
     _max = {'name': '', 'score': 0}
 
-    if a_dictionary is None or not a_dictionary:
+    if not a_dictionary:
         return None
-    for k, v in a_dictionary:
+    for k, v in a_dictionary.items():
         if _max['score'] < a_dictionary[k]:
             _max['name'] = k
             _max['score'] = v
+
     return _max['name']

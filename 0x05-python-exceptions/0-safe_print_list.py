19b45261807a3a16a0ba9347716cab4c347827d5
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-20 14:55:47 +0300

    Print safe list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+
+    try:
+        while i < x:
+            print(my_list[i], end='')
+            i += 1;
+        print()
+    except IndexError:
+        print()
+    finally:
+        return i
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
+
+
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,7 @@
+# Exceptions
+Dealing with execptions in python
+
+<details>
+<summary>0-safe\_print\_list</summary>
+python function prints x elements of a list.
+</details>

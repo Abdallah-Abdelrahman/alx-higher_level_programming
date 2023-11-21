773236130c730093b01ff736da06c1764d1b7820
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-22 00:55:10 +0300

    Fill w/ space 004

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,14 +26,14 @@ class Square:
     def my_print(self):
         """Print the sqaure."""
 
-        if (self.__position[1] > 0):
-            print()
-        for k in range((self.__position[0])):
-            print(' ', end='')
         if (not self.__size):
             print()
             return
+        for _ in range(self.__position[1]):
+            print()
         for i in range(self.__size):
+            for k in range((self.__position[0])):
+                print(' ', end='')
             for j in range(self.__size):
                 print('#', end='')
             print()

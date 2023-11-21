6c17bb2d150387e71cad257eb534c75f0a8ea0ad
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-21 21:32:03 +0300

    Define `area` instance method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
 
+"""This module define a class `Square`.
+"""
+
+
+class Square:
+    """Square defines private instance attribute `size`.
+    """
+
+    def __init__(self, size=0):
+        """Initialize the instance based on size.
+
+        Args:
+            size (int): size of the square.
+
+        Raises:
+            ValueError: size less than zero
+            TypeError: it's not int
+        """
+        if (type(size) is not int):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Calculate the area.
+
+        Returns:
+            area by raising size to the power 2
+        """
+        return self.__size ** 2

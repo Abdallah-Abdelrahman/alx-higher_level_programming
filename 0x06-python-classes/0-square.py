86b3236bb37491599708cc6e2aa96aff5a63769f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-21 20:10:35 +0300

    Define private instance attribute

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,6 +2,8 @@
 
 """This module define an empty class `Square`.
 """
+
+
 class Square:
     """Empty class.
     """
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""This module define a class `Square`.
+"""
+
+
+class Square:
+    """Square defines private instance attr.
+    """
+    def ___init__(self, size):
+        """Initialize the instance based on size.
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size

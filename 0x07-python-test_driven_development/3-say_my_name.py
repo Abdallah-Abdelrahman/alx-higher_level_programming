c385c3d2fb46b5f1f87dbf2ed57278164e662b2b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-28 22:34:55 +0300

    Print square test

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This module define say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints full name.
+
+    Args:
+        first_name: string
+        last_name: string
+    Raises:
+        TypeError: if neither of first_name nor last_name is a string
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print(f'My name is {first_name} {last_name}')
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This module defines print_square function"""
+
+
+def print_square(size):
+    """Prints a square.
+
+    Args:
+        size(int): the size of square.
+
+    Raises:
+        TypeError: size is not int.
+        ValueError: size < 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for _ in range(size):
+        print('#' * size)

19ea3af2b5644a7a7226ecf783e49d28154600e7
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-29 06:53:58 +0300

    Update string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""Module"""
-
-
 def magic_string():
-    """output"""
     magic_string.i = getattr(magic_string, 'i', 0) + 1
-    _str = ''
-
-    for j in range(magic_string.i):
-        _str += 'BestSchool' + ('', ', ')[j < magic_string.i - 1]
-    return _str
+    return ('BestSchool' + ('', ', ')[magic_string.i > 1]) * magic_string.i

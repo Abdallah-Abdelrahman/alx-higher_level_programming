94e53863e1b8d0a78aaf5da840e401a2d12fbe97
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-11-29 23:46:53 +0300

    Handle error cases for matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -16,26 +16,36 @@ def matrix_mul(m_a, m_b):
     err_type_a = 'm_a should contain only integers or floats'
     err_type_b = 'm_b should contain only integers or floats'
 
-    if _len_a == 0 or not (m_a[0]):
+    if _len_a == 0 or m_a[0] != 0 and not (m_a[0]):
         raise ValueError("m_a can't be empty")
-    if _len_b == 0 or not (m_b[0]):
+    if _len_b == 0 or m_a[0] != 0 and not (m_b[0]):
         raise ValueError("m_b can't be empty")
 
-    for i in range(len(m_a)):
-        if not isinstance(m_a[i], list):
+    for k, _ in enumerate(m_a):
+        if not isinstance(m_a[k], list):
             raise TypeError('m_a must be a list of lists')
+        if not m_a[k]:
+            raise ValueError("m_a can't be empty")
+        if not isinstance(m_b[k], list):
+            raise TypeError('m_b must be a list of lists')
+        if k and len(m_a[k]) != len(m_a[k - 1]):
+            raise TypeError('each row of m_a must be of the same size')
 
-        m_c.append([0] * len(m_b[0]))
-
-    for i in range(len(m_a)):
-        for j in range(len(m_b[0])):
-            for k in range(len(m_b)):
-                if not isinstance(m_b[k], list):
+        m_c.append([])
+        for i, _ in enumerate(m_b[k]):
+            _sum = 0
+            for j, _ in enumerate(m_b):
+                if j and len(m_b[j]) != len(m_b[j - 1]):
+                    raise TypeError('each row of m_b must be of the same size')
+                if not isinstance(m_b[i], list):
                     raise TypeError('m_b must be a list of lists')
-                if not isinstance(m_a[i][k], (int, float)):
+                if not m_b[i]:
+                    raise ValueError("m_b can't be empty")
+                if not isinstance(m_a[k][j], (int, float)):
                     raise TypeError(err_type_a)
-                if not isinstance(m_b[k][j], (int, float)):
+                if not isinstance(m_b[j][i], (int, float)):
                     raise TypeError(err_type_b)
+                _sum += m_a[k][j] * m_b[j][i]
+            m_c[k].append(_sum)
 
-                m_c[i][j] += m_a[i][k] * m_b[k][j]
     return m_c

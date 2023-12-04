7bc73391f3e27d9b1b9a09136ea27053c56df431
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-04 20:51:27 +0300

    Define `Square` class

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,4 +6,15 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ """
+    """inherits from Rectangle"""
+
+    def __init__(self, size):
+        """Initialize the instance"""
+
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Return the area"""
+
+        return self.__size ** 2

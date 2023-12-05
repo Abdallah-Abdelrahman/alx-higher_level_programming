47e9765e0d52d664a4f5cddb512d47414740276a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-06 02:41:17 +0300

    Write to json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Module defines save_to_json_file function"""
 
-from json import loads
+from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
-    """Write serialize pyhton obj to a file"""
+    """Write serialized pyhton obj to a file"""
 
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(loads(my_obj))
+        f.write(dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Module defines load_from_json_file function"""
 
-from json import dumps
+from json import loads
 
 
 def load_from_json_file(filename):
     """Dserialize obj from a file"""
 
     with open(filename, 'r', encoding='utf-8') as f:
-        return dumps(f.read())
+        return loads(f.read())

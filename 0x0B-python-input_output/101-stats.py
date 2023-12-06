24a766342905a0396a423b6de09f61f456065568
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-06 14:15:51 +0300

    Print stats

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,8 +1,48 @@
 #!/usr/bin/python3
-"""Module reads stdin line by line and computes metrics"""
+"""Module reads stdin line by line and computes metrics
+
+Attributes:
+    stat_dict: dictionary that holds the code status, and the size
+
+    Example:
+        >>> stat_dict = {'200': 1, '404': 2, '500': 3, 'size': 10}
+
+"""
 
 from sys import stdin
 
 
-for line in stdin:
-    print(line)
+if __name__ == '__main__':
+    stat_dict = {'size': 0}
+
+    for i, line in enumerate(stdin):
+        stat = line.split()
+        _len = len(stat)
+
+        if not stat[_len - 2] in stat_dict:
+            stat_dict[stat[_len - 2]] = 0
+
+        stat_dict[stat[_len - 2]] += 1
+        stat_dict['size'] += int(stat[_len - 1])
+
+        try:
+            if not (i + 1) % 10:
+                print('File size: {}'.format(stat_dict['size']))
+                for k, v in sorted(stat_dict.items()):
+                    if k == 'size':
+                        continue
+                    elif v:
+                        print('{}: {}'.format(k, v))
+                for k in stat_dict:
+                    if k != 'size':
+                        stat_dict[k] = 0
+        except KeyboardInterrupt:
+            if not (i + 1) % 10:
+                for k, v in sorted(stat_dict.items()):
+                    if k == 'size':
+                        print('File size: {}'.format(v))
+                    elif v:
+                        print('{}: {}'.format(k, v))
+                for k in stat_dict:
+                    if k != 'size':
+                        stat_dict[k] = 0

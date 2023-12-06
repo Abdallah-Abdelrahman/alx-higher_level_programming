9ffd7e841da8fd8d492baadfd10776d29632f6d1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-06 03:15:37 +0300

    Update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """Module defines class_to_json"""
 
-from json import dumps
-
 
 def class_to_json(obj):
     """Return class instance __dict__ in json format"""
 
-    return dumps(obj.__dict__)
+    return obj.__dict__

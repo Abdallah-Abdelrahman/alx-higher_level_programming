b1083d4e99895dd533c739ebb0cac2ce54c7f2f3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-06 03:13:55 +0300

    Return instance __dict__

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Module defines class_to_json"""
 
+from json import dumps
+
+
+def class_to_json(obj):
+    """Return class instance __dict__ in json format"""
+
+    return dumps(obj.__dict__)

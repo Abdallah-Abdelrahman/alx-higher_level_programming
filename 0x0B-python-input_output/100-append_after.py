e117a25abaede0216d625b5c32515f51489114d2
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-06 05:36:44 +0300

    Remove new line

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -11,4 +11,4 @@ def append_after(filename="", search_string="", new_string=""):
     with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             if search_string in line:
-                line += '\n' + new_string
+                line += new_string

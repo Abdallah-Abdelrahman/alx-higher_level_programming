f9119ca1699558f4bc635be557b102fae70c0fad
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-06 03:20:09 +0300

    Define a class `Student`

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""Modulue define Student class"""
 
+
+class Student:
+    """Define one isntance method"""
+
+    def __init__(self, first_name, last_name, age) -> None:
+        """Initialize the isntance """
+        self.first_name = first_name
+        self.las_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student"""
+        return self.__dict__

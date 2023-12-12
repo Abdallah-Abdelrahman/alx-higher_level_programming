77178189002f91f82de5c3d1f307c81b854ace0f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-12-12 04:57:26 +0300

    Fix failing test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,9 +2,12 @@
 """Unittest for `Base` class
 """
 
+import io
 import unittest
+from unittest.mock import patch
 import pep8
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -36,10 +39,22 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b4.id, 12)
         self.assertEqual(b5.id, 4)
 
-    def test_serialize_json(self):
-        """ """
-        pass
-
-    def test_deserialize_json(self):
-        """ """
-        pass
+    def test_to_json(self):
+        """Serialization test"""
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            _str = "{'x': 2, 'y': 8, 'id': 5, 'height': 7, 'width': 10}"
+            print(dictionary)
+            self.assertEqual(mock_stdout.getvalue().strip(), _str)
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            print(type(dictionary))
+            self.assertEqual(mock_stdout.getvalue(), "<class 'dict'>\n")
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            _str = '[{"x": 2, "y": 8, "id": 5, "height": 7, "width": 10}]'
+            print(json_dictionary)
+            self.assertEqual(mock_stdout.getvalue().strip(), _str)
+        with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            print(type(json_dictionary))
+            self.assertEqual(mock_stdout.getvalue(), "<class 'str'>\n")
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -23,12 +23,12 @@ class TestRectangle(unittest.TestCase):
     def test_init1(self):
         """Test"""
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 5)
+        self.assertEqual(r1.id, 6)
 
     def test_init2(self):
         """Test"""
         r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 6)
+        self.assertEqual(r2.id, 7)
 
     def test_init3(self):
         """Test"""
@@ -144,10 +144,10 @@ class TestRectangle(unittest.TestCase):
         r1 = Rectangle(10, 2, 1, 9)
         r1_dictionary = r1.to_dictionary()
         with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
+            _str = "{'x': 1, 'y': 9, 'id': 13, 'height': 2, 'width': 10}"
+
             print(r1_dictionary)
-            self.assertEqual(mock_stdout.getvalue().strip(),
-                             "{'x': 1, 'y': 9, 'id': 12, 'height': 2,\
-                                     'width': 10}")
+            self.assertEqual(mock_stdout.getvalue().strip(), _str)
         with patch('sys.stdout', new_callable=io.StringIO) as mock_stdout:
             print(type(r1_dictionary))
             self.assertEqual(mock_stdout.getvalue().strip(), "<class 'dict'>")

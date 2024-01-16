6c7f71f4e2d7acf1a5d3f02f86008d4d19536cfc
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-16 23:44:50 +0300

    List all records in `second_table` table that have a name value

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score,
 -- in the table second_table of the database hbtn_0c_0
-SELECT score, COUNT(score) AS 'number' FROM second_table
+SELECT score, COUNT(*) AS 'number' FROM second_table
 GROUP BY score, 'number'
 ORDER BY 'number' DESC;

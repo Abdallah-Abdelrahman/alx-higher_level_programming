3276715c97f3f3783bd3e9c89a16fb9dca11ddf7
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-16 22:38:18 +0300

    Double quotes around the names

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,11 +3,11 @@ CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
-)
+);
 -- Insert into the table
 INSERT INTO second_table (id, name, score)
 VALUES
-(1, 'Jon', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8)
+(1, "Jon", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);

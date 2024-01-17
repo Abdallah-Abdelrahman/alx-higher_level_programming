c23b866e62ad7ae04706faa89ec98a9c5dbefab8
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 02:55:23 +0300

    Update use creation

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,7 +3,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2; -- CREATE db
 
 -- CREATE USER
-CREATE USER user_0d_2@localhost
+CREATE USER IF NOT EXISTS user_0d_2@localhost
 IDENTIFIED by 'user_0d_2_pwd';
 
 -- assign privileges

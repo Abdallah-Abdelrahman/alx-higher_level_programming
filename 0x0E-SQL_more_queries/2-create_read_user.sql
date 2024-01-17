606cca85056a9dd0bdb446bfd43436500c4ed58a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 02:52:14 +0300

    Update user privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,9 +4,9 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2; -- CREATE db
 
 -- CREATE USER
 CREATE USER user_0d_2@localhost
-IDENTIFIED by user_0d_2_pwd;
+IDENTIFIED by 'user_0d_2_pwd';
 
 -- assign privileges
 GRANT SELECT
-ON hbtn_0d_2
+ON hbtn_0d_2.*
 TO user_0d_2@localhost;

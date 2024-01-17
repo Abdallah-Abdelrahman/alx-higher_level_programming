7716a67667902535c4d81ace38faee15f5bb9eff
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 02:42:39 +0300

    create db and new user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- creates the DATABASE hbtn_0d_2 AND the USER user_0d_2.
+
+CREATE DATABASE hbtn_0d_2; -- CREATE db
+
+-- CREATE USER
+CREATE USER user_0d_2@localhost
+IDENTIFIED by user_0d_2_pwd;
+
+-- assign privileges
+GRANT SELECT
+ON hbtn_0d_2
+TO user_0d_2@localhost;

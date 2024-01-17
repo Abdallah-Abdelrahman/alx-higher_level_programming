7ced1ff9d3a78030ae921db7b1b620928d1a188f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 02:47:19 +0300

    Create new table `force_name`

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates the DATABASE hbtn_0d_2 AND the USER user_0d_2.
 
-CREATE DATABASE hbtn_0d_2; -- CREATE db
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2; -- CREATE db
 
 -- CREATE USER
 CREATE USER user_0d_2@localhost

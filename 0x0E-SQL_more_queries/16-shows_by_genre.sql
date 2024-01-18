e2120f7886dc4ef8d002250f0609f67f86b20aa4
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 05:49:06 +0300

    Add full join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- script that lists ALL shows, AND ALL genres linked TO that SHOW, FROM the DATABASE hbtn_0d_tvshows
 SELECT s.title, g.name
 FROM tv_shows AS s
-OUTER JOIN tv_show_genres AS sg
+FULL OUTER JOIN tv_show_genres AS sg
 ON s.id = sg.show_id
-OUTER JOIN tv_genres AS g
+FULL OUTER JOIN tv_genres AS g
 ON g.id = sg.genre_id
 ORDER BY s.title, g.name ASC

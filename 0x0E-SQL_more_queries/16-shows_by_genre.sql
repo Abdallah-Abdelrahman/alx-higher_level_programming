0782a0b6190f68fb9c9e343af86dd82ae6282f32
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 05:46:07 +0300

    Capitalize outer

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- script that lists ALL shows, AND ALL genres linked TO that SHOW, FROM the DATABASE hbtn_0d_tvshows
 SELECT s.title, g.name
 FROM tv_shows AS s
-outer JOIN tv_show_genres AS sg
+OUTER JOIN tv_show_genres AS sg
 ON s.id = sg.show_id
-outer JOIN tv_genres AS g
+OUTER JOIN tv_genres AS g
 g.id = sg.genre_id
 ORDER BY s.title, g.name ASC

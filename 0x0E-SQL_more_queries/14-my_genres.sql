f126f5c37affb133e2c753ab44a17a5f2a28e9a1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 05:35:41 +0300

    Add another join

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,7 @@ SELECT g.name FROM
 tv_genres AS g
 JOIN tv_show_genres AS sg
 ON g.id = sg.genre_id
-WHERE (SELECT title FROM tv_shows
-	WHERE name = 'Dexter'
-)
+JOIN tv_shows AS s
+ON s.id = sg.show_id
+WHERE s.title = 'Dexter'
 ORDER BY g.name ASC

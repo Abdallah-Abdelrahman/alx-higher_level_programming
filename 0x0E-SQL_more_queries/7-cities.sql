989b8a45f57888bc05ac498afcdb7781ca723d28
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 04:00:14 +0300

    y

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,12 @@
 -- creates the DATABASE hbtn_0d_usa AND the TABLE cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- CREATE TABLE
+-- create table
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL UNIQUE AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (state_id)
-		REFERENCES hbtn_0d_usa.states(id);
+		REFERENCES hbtn_0d_usa.states(id)
 );

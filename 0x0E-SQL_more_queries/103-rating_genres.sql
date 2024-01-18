1a48f7f06ee4ea664e7a935ba8d9e3ecaedfdfac
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 07:57:09 +0300

    List genres w/ rating

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,10 @@
+-- lists ALL genres IN the DATABASE hbtn_0d_tvshows_rate by their rating
+SELECT g.name, SUM(r.rate) AS `rating`
+FROM tv_genres AS g
+JOIN tv_show_genres AS sg
+ON g.id = sg.genre_id
+JOIN tv_shows AS s
+ON s.id = sg.show_id
+JOIN r
+ON s.id = r.show_id
+ORDER BY `rating` DESC;

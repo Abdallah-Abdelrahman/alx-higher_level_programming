2e288ee962ed4e5093f3624fbd7f597bbe89d1a6
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 03:03:09 +0300

    Create new table and db

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- creates the DATABASE hbtn_0d_usa AND the TABLE states (in the DATABASE hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- CREATE TABLE
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+	id INT UNIQUE NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);

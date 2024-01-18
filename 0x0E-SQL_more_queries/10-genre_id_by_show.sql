6c748bdb1617692620cda003905e662f0603c62e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 04:25:49 +0300

    tv 000

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- lists ALL shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT t1.title, t2.genre_id
+FROM tv_shows AS t1
+JOIN tv_show_genres AS t2
+ON t1.id = t2.genre_id
+ORDER BY t1.title, t2.genre_id

f11ebb05112a55d0129cf981ac6b14c783fb9a8c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 05:25:49 +0300

    Add alias

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists ALL genres FROM hbtn_0d_tvshows AND displays the number of shows linked TO each
 SELECT g.name AS `genre`, COUNT(sg.show_id) AS `number_of_shows`
-FROM tv_genres
+FROM tv_genres AS g
 JOIN tv_show_genres AS sg
 ON g.id = sg.genre_id
 GROUP BY `genre`

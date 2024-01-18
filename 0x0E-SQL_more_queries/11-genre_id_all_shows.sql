61ce6abfd7528be2876d6eabeeaa2214d4389bfe
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 04:34:36 +0300

    list shows with left outer join

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists ALL shows contained in the DATABASE hbtn_0d_tvshows
+SELECT t1.title, t2.genre_id
+FROM tv_shows AS t1
+LEFT OUTER JOIN tv_show_genres AS t2
+ON t1.id = t2.genre_id
+ORDER BY t1.title, t2.genre_id

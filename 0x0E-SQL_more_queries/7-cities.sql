35693e67890e83c725dbd2e9eac4fdc45ac49319
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 03:30:57 +0300

    update foreign key 000

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,6 @@ CREATE TABLE hbtn_0d_usa.cities (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(state_id)
+	FOREIGN KEY (state_id)
+		REFERENCES hbtn_0d_usa.states(hbtn_0d_usa.states.id);
 );

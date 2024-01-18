3817072fe6fd7539b38a18590975f6e22706972d
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 03:15:18 +0300

    Create table `cities` with pk and fk

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- creates the DATABASE hbtn_0d_usa AND the TABLE cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- CREATE TABLE
+CREATE TABLE hbtn_0d_usa.cities (
+	id INT UNIQUE NOT NULL AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
+);

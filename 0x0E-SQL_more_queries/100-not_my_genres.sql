8e070fdcdd872ac985e90c548ff75d0368783330
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 06:54:50 +0300

    Fix where clause

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,11 @@
 -- script that uses the hbtn_0d_tvshows DATABASE TO list ALL genres NOT linked TO the SHOW Dexter
 SELECT g.name
 FROM tv_genres AS g
-WHERE NOT IN (
-	SELECT s.title
+WHERE g.id NOT IN (
+	SELECT sg.genre_id
 	FROM tv_shows AS s
 	JOIN tv_show_genres AS sg
 	ON s.id = sg.show_id
-	s.title = 'Dexter'
-);
-ORDER BY g.name ASC
+	WHERE s.title = 'Dexter'
+)
+ORDER BY g.name ASC;

4fce651fca9c0ea4089c3a63fd87b994375b7f37
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 05:17:54 +0300

    List genres

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- lists ALL genres FROM hbtn_0d_tvshows AND displays the number of shows linked TO each
+SELECT g.name AS `genre`, COUNT(sg.show_id) AS `number_of_shows`
+FROM tv_genres
+JOIN tv_show_genres AS sg
+ON g.id = sg.genre_id
+GROUP BY `genre`
+ORDER BY `number_of_shows` DESC

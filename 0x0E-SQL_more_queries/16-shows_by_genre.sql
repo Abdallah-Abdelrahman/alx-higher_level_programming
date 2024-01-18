34b342dd94ae4de16bcb31daf9f9dfe858cff10f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 05:53:12 +0300

    Use left join for both

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT s.title, g.name
 FROM tv_shows AS s
 LEFT OUTER JOIN tv_show_genres AS sg
 ON s.id = sg.show_id
-RIGHT JOIN tv_genres AS g
+LEFT JOIN tv_genres AS g
 ON g.id = sg.genre_id
 ORDER BY s.title, g.name ASC

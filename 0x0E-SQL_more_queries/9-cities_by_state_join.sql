3c8d00147c0a2635e960d898b62df8be12e7ab56
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 04:19:37 +0300

    join 001

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists ALL cities contained in the DATABASE hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM cities AS c
-LEFT OUTER JOIN states AS s
+JOIN states AS s
 ON c.state_id = s.id
 ORDER BY c.id ASC;

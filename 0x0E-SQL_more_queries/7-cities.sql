ba3af02c2800e22a0aafe8fd54a410c4bbd13433
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 03:52:16 +0300

    fk 003

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL UNIQUE AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	PRIMARY KEY (id),
+	name VARCHAR(256),
 	FOREIGN KEY (state_id)
 		REFERENCES states(id);
 );

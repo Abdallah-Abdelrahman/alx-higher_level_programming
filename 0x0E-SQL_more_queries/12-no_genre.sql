9ede998f0bfde48f742e4e9dfc63c369594c96d0
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 04:47:43 +0300

    list show that doesn't have genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- lists ALL shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT t1.title, t2.genre_id
+FROM tv_shows AS t1
+JOIN tv_show_genres AS t2
+ON t1.id = t2.show_id
+WHERE t2.id IS NULL
+ORDER BY t1.title, t2.genre_id

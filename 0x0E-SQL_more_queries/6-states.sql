66d28235f7b1cabe82c801392ccab9506e9538e3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 15:51:26 +0300

    Refactor constraint convention

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
--- creates the DATABASE hbtn_0d_usa AND the TABLE states (in the DATABASE hbtn_0d_usa)
+-- creates the DATABASE hbtn_0d_usa AND the TABLE states (IN the DATABASE hbtn_0d_usa)
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- CREATE TABLE
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+	CONSTRAINT `hbtn_0d_usa.states_pk` PRIMARY KEY (id)
 );

62d99ef03998e0fa73048e0c8f07743b3fc6c45c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-01-18 05:50:35 +0300

    Use left and right join

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- script that lists ALL shows, AND ALL genres linked TO that SHOW, FROM the DATABASE hbtn_0d_tvshows
 SELECT s.title, g.name
 FROM tv_shows AS s
-FULL OUTER JOIN tv_show_genres AS sg
+LEFT OUTER JOIN tv_show_genres AS sg
 ON s.id = sg.show_id
-FULL OUTER JOIN tv_genres AS g
+RIGHT JOIN tv_genres AS g
 ON g.id = sg.genre_id
 ORDER BY s.title, g.name ASC

e3fee8a2d55b3d650928a18f1e40ff6c2f0b8146
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-12 10:05:27 +0300

    Print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+/** prints a square.
+ * The first argument is the size of the square
+ * If the first argument can’t be converted to an integer, print “Missing size”
+ */
+const [, , arg] = process.argv;
+const n = parseInt(arg);
+let s = '';
+
+if (isNaN(n)) console.log('Missing size');
+else if (n >= 0) {
+  for (let i = 0; i < n; i++, s += i < n ? '\n' : '') {
+    for (let j = 0; j < n; j++) {
+      s += 'X';
+    }
+  }
+  console.log(s);
+}

37edf58758cf7f4232096a49621cd91579aafeef
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-12 11:41:08 +0300

    Export function add

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/** @module 13-add.js */
+
+/**
+ * sums to integers
+ * @function
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+module.exports.add = function (a, b) {
+  return (a + b);
+};

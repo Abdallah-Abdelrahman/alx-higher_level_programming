c55cfc48b6e2b613f7fddfdb4475780c06665126
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-12 12:18:46 +0300

    Update callback name

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-/** @module 102-add_me_maybe.js */
-
 /**
+ * @module 102-add_me_maybe.js
  * increments and calls a function.
  * @function
  * @param {number} n
  * @param {function} cb
  */
-module.exports.addMeMaybe = function (n, cb) {
-  cb(n + 1);
+module.exports.addMeMaybe = function (n, theFunction) {
+  theFunction(n++);
 };

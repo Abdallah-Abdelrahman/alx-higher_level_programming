8b3fb3c1443d43d3ab716ffa6ae80ef558cc3245
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-12 08:10:50 +0300

    Initial commit

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// create constant variable and print it
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints the first argument passed to it:
+const { argv } = process;
+
+if (!argv[2])
+	console.log('No argument');
+else
+	console.log(argv[2]);

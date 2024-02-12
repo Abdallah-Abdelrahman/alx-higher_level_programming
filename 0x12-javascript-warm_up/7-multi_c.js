234e64375556af7808627a86ba14be45c6b370bf
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-12 09:24:30 +0300

    Print string n times where n 1st argument to the script

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+// prints x times “C is fun”
+const { argv } = process;
+const n = parseInt(argv[2]);
+
+if (isNaN(n)) console.log('Missing number of occurrences');
+else if (n >= 0) [...Array(n)].forEach(_ => console.log('C is fun'));

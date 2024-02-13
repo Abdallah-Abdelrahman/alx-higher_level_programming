fb455a23f786678e01a515dc937c3769ad2dfdd8
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-13 07:22:28 +0300

    Add instance methods `print, rotate, double` to Rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,41 @@
 #!/usr/bin/node
+/** Class represents Rectangle */
+module.exports = class Rectangle {
+  /**
+   * Create a Rectangle
+   * @param {number} w - width
+   * @param {number} h - height
+   */
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  /** Print a Rectangle */
+  print () {
+    let s = '';
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++, s += i < this.height ? '\n' : '') {
+        for (let j = 0; j < this.width; j++) {
+          s += 'X';
+        }
+      }
+    }
+    console.log(s);
+  }
+
+  /** Exchange width and height of the Rectangle */
+  rotate () {
+    this.width ^= this.height;
+    this.height ^= this.width;
+    this.width ^= this.height;
+  }
+
+  /** Multiply width and height by 2 */
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};

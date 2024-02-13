fcefd45aacbdc39e359009753cfaf89ca8977792
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-13 09:05:28 +0300

    Print number of arguments already printed

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let count = 0;
+
+/**
+ * prints the number of arguments already printed and the new argument value
+ * @param {*} item - any type
+ */
+exports.logMe = function (item) {
+  console.log(`${count++}: ${item}`);
+};

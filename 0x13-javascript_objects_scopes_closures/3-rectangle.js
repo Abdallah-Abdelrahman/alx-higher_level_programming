f298190346d516dd209df6073f9c5c9b52902690
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-13 07:04:16 +0300

    Print Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let s = '';
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++, s += i < this.height ? '\n' : '') {
+        for (let j = 0; j < this.width; j++) {
+          s += 'X';
+        }
+      }
+    }
+    console.log(s);
+  }
+};

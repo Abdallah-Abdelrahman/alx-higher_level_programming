98cae060bc67abb5e6febb4101ea1100f8e4a5b3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-13 18:58:34 +0300

    Add jsdoc

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/**
+ * @module 102-concat
+ * 1st and 2nd arguments to the program are source files to read from,
+ * 3rd argument is the distination file to append the data to
+ */
 const { readFileSync, appendFileSync } = require('fs');
 const { 2: src1, 3: src2, 4: dist } = process.argv;
 

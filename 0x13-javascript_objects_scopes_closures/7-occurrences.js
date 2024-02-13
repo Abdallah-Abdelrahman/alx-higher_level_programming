07728a2554dab78a2f2d272c49a5c70b1b2b459b
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-13 08:28:36 +0300

    Count number of occurrences of element in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/**
+ * returns the number of occurrences in a list.
+ * @param {Array.<number>} list
+ * @param {number} searchElement
+ * @return {number} number of occurrences
+ */
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  if (!list) return (0);
+  for (const el of list) {
+    if (el === searchElement) count++;
+  }
+  return (count);
+};

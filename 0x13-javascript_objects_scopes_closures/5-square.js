24340e30950d921a45d748a833b90949396aa66a
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-13 07:33:00 +0300

    Add class `Square`

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+/**
+ * Class reperesents Square.
+ * @extends Rectangle
+ */
+module.exports = class Square extends Rectangle {
+  /**
+   * Create a Square
+   * @param {number} size
+   */
+  constructor (size) {
+    super(size, size);
+  }
+};

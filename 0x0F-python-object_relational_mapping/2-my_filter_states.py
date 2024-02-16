5bec4b576fda9c0a65b7936d8d774d6875a5fa36
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-16 10:55:50 +0300

    Use try/except

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,14 +11,16 @@ from sys import argv
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    query = """SELECT * FROM states
-                WHERE name = '{}'
-                ORDER BY id;""".format(argv[4])
-    cur.execute(query)
-    rows = cur.fetchall()
-    if rows:
+    try:
+        cur = conn.cursor()
+        query = """SELECT * FROM `states`
+                    WHERE `states`.name = '{:s}'
+                    ORDER BY `states`.id ASC;""".format(argv[4])
+        cur.execute(query)
+        rows = cur.fetchall()
         for r in rows:
             print(r)
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+    except Exception:
+        pass

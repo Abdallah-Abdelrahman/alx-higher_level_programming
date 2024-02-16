88875645166a5840832eb997be2bf39552ae4384
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-16 10:23:09 +0300

    Add semicolon

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,9 +14,10 @@ if __name__ == '__main__':
     cur = conn.cursor()
     query = """SELECT * FROM states
                 WHERE name = '{}'
-                ORDER BY id""".format(argv[4])
+                ORDER BY id;""".format(argv[4])
     cur.execute(query)
-    row = cur.fetchone()
-    print(row)
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
     cur.close()
     conn.close()

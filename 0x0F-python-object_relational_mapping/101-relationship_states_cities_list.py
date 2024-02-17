5884b08d2caf84c8b5ab93f2186fdf7a02f4ab2f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-17 18:54:42 +0300

    Comment tables creation

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,8 +4,8 @@ contained in the database hbtn_0e_101_usa.
 '''
 
 import sys
-from relationship_state import Base, State
-from relationship_city import City
+from relationship_state import State
+from relationship_city import City as _
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -15,10 +15,10 @@ if __name__ == '__main__':
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-    Base.metadata.create_all(engine)
+    # Base.metadata.create_all(engine)
     # sessionmaker function returns `Session` class
     session = sessionmaker(bind=engine)()
-    for state in session.query(State):
+    for state in session.query(State).all():
         print('{}: {}'.format(state.id, state.name))
-        print('\n'.join(['    {}: {}'.format(city.id, city.name)
+        print('\n'.join(['\t{}: {}'.format(city.id, city.name)
                          for city in state.cities]))

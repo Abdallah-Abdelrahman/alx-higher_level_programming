df1da028805f0efadd928fc31b93fc8857ba5598
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-17 17:45:06 +0300

    Select all state objects using `State.cities` relational field

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa.
+'''
+
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    # sessionmaker function returns `Session` class
+    session = sessionmaker(bind=engine)()
+    for state in session.query(State)\
+            .join(State.cities).order_by(State.id, City.id):
+        print('{}: {}'.format(state.id, state.name))
+        print('\n'.join(['\t{}: {}'.format(city.id, city.name)
+                         for city in state.cities]))

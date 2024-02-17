25c9aa95cef169bf4425967ee8220f4f3017afda
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-02-17 09:16:18 +0300

    Add class `City` and query cities for each state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''class definition of a City'''
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    '''class City maps to `cities` table
+
+    Attrs:
+        __tablename__: table's name
+        id: city's id
+        name: city's name
+        state_id: foreign key to states.id
+    '''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))

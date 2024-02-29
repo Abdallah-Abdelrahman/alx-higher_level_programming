bcc514ca80bbe049c7c4ba1d8a423f45daff0035
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 02:32:58 +0300

    Make scripts 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
-stat_code=$(curl -Is "$1" | awk '/HTTP/ {print $2}')
-
-[ "$stat_code" -eq 200 ] && curl -s "$1"
+curl -s "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-
-curl -Is | awk '/Allow:/ {print $2}'
+curl -I -s -X OPTIONS | awk '/Allow:/ {print $2}'

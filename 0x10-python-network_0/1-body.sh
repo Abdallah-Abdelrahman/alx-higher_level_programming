17ecbc6886f81ee2f834481bf76fc12a6bbf59a3
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 02:17:32 +0300

    Update 002-1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 
-curl -sL "$1"
+stat_code=$(curl -Is "$1" | awk '/HTTP/ {print $2}')
+
+[ "$stat_code" -eq 200 ] && curl -s "$1"

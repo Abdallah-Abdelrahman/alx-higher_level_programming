d443d3ebe8a37c13f8c4dd2e34a287e0dcb031ba
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 01:56:53 +0300

    Display body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,6 +2,5 @@
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 # shellcheck disable=SC1083
 
-res=$(curl -sw %{http_code} "$1" -o /dev/null)
 
-[ "$res" -eq 200 ] && curl -s "$1" 
+curl -s "$1"

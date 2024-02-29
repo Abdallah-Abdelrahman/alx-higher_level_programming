196ea3d0bb6ab90aa5420c6bc00f787adf516eab
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 01:55:09 +0300

    Display body of response when 200 status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# shellcheck disable=SC1083
+
+res=$(curl -sw %{http_code} "$1" -o /dev/null)
+
+[ "$res" -eq 200 ] && curl -s "$1" 

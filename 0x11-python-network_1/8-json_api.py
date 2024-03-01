e603ffe7e0e6da5f4bf84e3bb2f2876f9fca1cf1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-02 02:17:26 +0300

    Send `query` parameter with the request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+'''script that takes in a letter and sends a POST request to,
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+'''
+import sys
+from requests import get, exceptions
+
+
+if __name__ == '__main__':
+    q = sys.argv[2] if len(sys.argv) > 2 else ""
+    url = 'http://0.0.0.0:5000/search_user?'
+    try:
+        req = get(url, params={'q': q})
+        json = req.json()
+        print('No result' if len(json) == 0 else '[{}] {}'
+              .format(json['id'], json['name']))
+    except exceptions.JSONDecodeError:
+        print('Not a valid JSON')
+    except exceptions.HTTPError:
+        pass

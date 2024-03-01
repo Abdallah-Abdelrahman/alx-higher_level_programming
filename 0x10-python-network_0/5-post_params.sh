aa85c31e64ea68e21a0226aa811f3a7b24cfe645
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 03:11:54 +0300

    Update 001-5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X POST -d 'email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD'
+curl -X POST -d 'email=test%40gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD'

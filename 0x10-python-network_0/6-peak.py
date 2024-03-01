e7a94a2f4c1c62dfefb44dd88f3ec076c9a80c8f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 04:32:10 +0300

    Find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''Module defines `find_peak` function'''
+
+
+def find_peak(list_of_integers):
+    '''Finds a peak in a list of unsorted integers.'''
+
+    if not list_of_integers:
+        return None
+    _len = len(list_of_integers)
+    if _len == 1:
+        return list_of_integers[0]
+    # check for every other element
+    for i in range(1, _len - 1):
+        # check if the neighbors are smaller
+        if (list_of_integers[i] >= list_of_integers[i - 1] and
+                list_of_integers[i] >= list_of_integers[i + 1]):
+            return list_of_integers[i]

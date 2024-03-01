a8a39fddd4628bdd04adf4c00222dbb5dae80904
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 03:50:56 +0300

    Update 004-3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I -X OPTIONS "$1" | awk '/allow-methods/ {print $2}'
+curl -s -I "$1" | awk '/Allow:/ {print $2}'

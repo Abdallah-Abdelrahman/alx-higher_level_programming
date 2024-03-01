bb1f7868a499083f65c6283a3d40ddfbba55290f
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 22:40:50 +0300

    Manage `HTTPError` exception

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,5 +10,5 @@ from urllib import request, parse
 if __name__ == '__main__':
     data = parse.urlencode({'email': sys.argv[2]})
     req = request.Request(sys.argv[1], data.encode('ascii'))
-    with request.urlopen(sys.argv[1]) as res:
+    with request.urlopen(req) as res:
         print(res.read().decode('utf8'))

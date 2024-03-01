1e55580211c8aeb1874f942cafd7591fb2200a12
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 21:53:03 +0300

    Fix `pycodestyle`

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 '''Script that fetches https://alx-intranet.hbtn.io/status.
-
-- You must use the package urllib
-- You are not allowed to import any packages other than urllib
-- You must use a with statement
 '''
 from urllib import request
 
@@ -11,6 +7,7 @@ from urllib import request
 if __name__ == '__main__':
     with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         html = res.read()
-        print('Body response:\n\t- type: {}\
-              \n\t- content: {}\n\t- utf8 content: {}'
-              .format(type(html), html, html.decode('utf8')))
+        s = 'Body response:\n'
+        s += '\t- type: {}\n\t- content: {}\n\t- utf8 content: {}'.format(
+                type(html), html, html.decode('utf8'))
+        print(s)

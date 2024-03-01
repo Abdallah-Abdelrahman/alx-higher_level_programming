f22cbff22b9e4602574103c42d4e3a3b2844b2e4
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-01 21:37:51 +0300

    Fetch basic response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''Script that fetches https://alx-intranet.hbtn.io/status.
+
+- You must use the package urllib
+- You are not allowed to import any packages other than urllib
+- You must use a with statement
+'''
+from urllib import request
+
+
+if __name__ == '__main__':
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
+        print('Body response:\n\t- type: {}\
+              \n\t- content: {}\n\t- utf8 content: {}'
+              .format(type(html), html, html.decode('utf8')))

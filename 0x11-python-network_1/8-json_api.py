bf54c177ca2427c7833665c1efc7dafa166ab801
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-02 04:01:00 +0300

    Update 001-8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,14 +3,14 @@
 http://0.0.0.0:5000/search_user with the letter as a parameter.
 '''
 import sys
-from requests import get, exceptions
+from requests import post, exceptions
 
 
 if __name__ == '__main__':
     q = sys.argv[2] if len(sys.argv) > 2 else ""
     url = 'http://0.0.0.0:5000/search_user'
     try:
-        req = get(url, params={'q': q})
+        req = post(url, params={'q': q})
         req.raise_for_status()
         json = req.json()
         print('No result' if len(json) == 0 else '[{}] {}'

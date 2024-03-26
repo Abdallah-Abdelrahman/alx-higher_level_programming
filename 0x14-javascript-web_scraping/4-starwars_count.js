f932298ed0de9f089e36a5d054b9914096e6ea61
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-27 02:07:46 +0300

    Update 004-4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,19 +2,17 @@
 const request = require('request');
 const argv = process.argv;
 
-if (argv[2]) {
-  request(argv[2], function (error, response, body) {
-    if (error) { return console.log(error); }
+request.get(argv[2], function (error, response, body) {
+  if (error) { return console.log(error); }
 
-    if (response.statusCode === 200) {
-      const films = JSON.parse(body).results;
-      let count = 0;
-      for (const { characters } of films) {
-        if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-          count++;
-        }
+  if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const { characters } of films) {
+      if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
       }
-      console.log(count);
     }
-  });
-}
+    console.log(count);
+  }
+});

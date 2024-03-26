e4058262f98e03b2b0746bd9477e83ea58bb07a6
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-27 01:22:30 +0300

    Update 000-4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,10 +7,10 @@ request(argv[2], function (error, response, body) {
   if (response.statusCode === 200) {
     const films = JSON.parse(body).results;
     const number = films.reduce((acc, { characters }) => {
-      return characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+      return (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
         ? acc + 1
-        : acc;
+        : acc);
     }, 0);
-    console.log(number);
+    console.log(number.toString());
   }
 });

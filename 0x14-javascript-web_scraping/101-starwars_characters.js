b036cad613e936a7503fff0b47aafb9e824ce083
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-27 01:13:38 +0300

    Use async generator function

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,14 +3,25 @@ const request = require('request');
 const { argv } = process;
 const api = 'https://swapi-api.alx-tools.com/api';
 
-request(`${api}/films/${argv[2]}`, function (_error, response, body) {
+request(`${api}/films/${argv[2]}`, async function (_error, response, body) {
   const characters = [];
   if (response.statusCode === 200) {
     characters.push(...JSON.parse(body).characters);
-    for (const character of characters) {
-      request(character, function (_error, _response, body) {
-        console.log(JSON.parse(body).name);
-      });
+    for await (const name of charactersGenerator(characters)) {
+      console.log(name);
     }
   }
 });
+
+async function * charactersGenerator (chars) {
+  for (const ch of chars) {
+    yield new Promise((resolve, reject) => {
+      request(ch, function (error, response, body) {
+        if (error) { reject(error); }
+        if (response.statusCode === 200) {
+          resolve(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+}

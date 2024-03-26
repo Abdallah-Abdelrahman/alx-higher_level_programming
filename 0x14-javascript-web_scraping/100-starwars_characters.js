3e285643e45d6f67933ed5a28359b628fcbde493
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-27 00:15:32 +0300

    Get characters for movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const { argv } = process;
+const api = 'https://swapi-api.alx-tools.com/api';
+
+request(`${api}/films/${argv[2]}`, function (_error, response, body) {
+  const characters = [];
+  if (response.statusCode === 200) {
+    characters.push(...JSON.parse(body).characters);
+    for (const character of characters) {
+      request(character, function (_error, _response, body) {
+        console.log(JSON.parse(body).name);
+      });
+    }
+  }
+});
